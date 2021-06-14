d3185d75b315603337665a443401d98ccd7f2732
mafei <mafei.dev@gmail.com>
2021-06-14 18:42:30 +0530

    Initial commit

---
+++ src/test/java/com/mafei/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mafei.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
